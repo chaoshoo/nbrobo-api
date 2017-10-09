8b9ad5f0aee32479d599d770bbb41ec447152a71
chaos <chaos.hoo@gmail.com>
2017-10-09 18:29:12 +0800

    调试挂号接口

--- src/com/net/ServiceConstants.java
+++ src/com/net/ServiceConstants.java
@@ -5,14 +5,15 @@ import java.util.ResourceBundle;
 
 /**
  * 服务常量
+ * 
  * @author hiveview
  *
  */
 public class ServiceConstants {
 	private static ResourceBundle conf = ResourceBundle.getBundle("conf");
 
-	public static String appid="HYGH000001";
-	public static String key="7b9cbbfe355d97cc5de5cb9d3f3ac62f";
+	public static String appid = "HYGH000004";
+	public static String key = "7b9cbbfe355d97cc5de5cb9d3f3ac62f";
 	public static String jdbc_driver = conf.getString("jdbc.driver");
 	public static String jdbc_url = conf.getString("jdbc.url");
 	public static String jdbc_user = conf.getString("jdbc.user");
@@ -21,17 +22,34 @@ public class ServiceConstants {
 	public static final String URL_DOMAIN = "http://www.nky.com/";
 
 	public static final String URL_UPLOAD = "";
-	public static final String HOSPITALURL = "http://27.17.40.149:9000/hy_ghservice/ghapi/flag?";
-	public static final String DOCTORURL = "http://27.17.40.149:9000/hy_ghservice/ghdoctor/flag?";
-	public static final String CENTERGH= "http://27.17.40.149:9000/hy_ghservice/centergh/flag?";
-	public static final String CIPHERURL="http://27.17.40.149:9000/hy_ghservice/centergh/jiami?miwen=";
-	public static final String WEIXIN_PUSH="http://wx.nbrobo.com/wxPush/send.json?";
-	public static final String WEIXIN_MD5KEY="nky_0654321" ;//  如果要修改  对应微信也要修改
-	
-	
+
+	public static final String HY_URL_DOMAIN = "http://27.17.40.149:9000/hy_ghservice";
+	public static final String HY_URL_DOMAIN_API = HY_URL_DOMAIN + "/ghapi";
+	public static final String HY_URL_DOMAIN_DOCTOR = HY_URL_DOMAIN + "/ghdoctor";
+	public static final String HY_URL_DOMAIN_CENTER = HY_URL_DOMAIN + "/centergh";
+
+	public static final String HY_URL_HOSPITAL = HY_URL_DOMAIN_API + "/gethospital";
+	public static final String HY_URL_DEPARTMENT = HY_URL_DOMAIN_API + "/getdept";
+	public static final String HY_URL_DOCTOR = HY_URL_DOMAIN_API + "/Ghgetdoctor";
+	public static final String HY_URL_DOCTOR_PARTTIME_SHCEDULE = HY_URL_DOMAIN_API + "/GetPartTime";
+
+	public static final String HY_URL_DOCTOR_SHCEDULE = HY_URL_DOMAIN_DOCTOR + "/getDoctorSchedule";
+	public static final String HY_URL_DOCTOR_STOP = HY_URL_DOMAIN_DOCTOR + "/getDoctorstop";
+
+	public static final String HY_URL_LOCK = HY_URL_DOMAIN_CENTER + "/ghlock";
+	public static final String HY_URL_PLACE_ORDER = HY_URL_DOMAIN_CENTER + "/confirmOrder";
+	public static final String HY_URL_CANCEL_ORDER = HY_URL_DOMAIN_CENTER + "/cancelorder";
+	public static final String HY_URL_CIPHER = HY_URL_DOMAIN_CENTER + "/jiami?miwen=";
+
+	public static final String HY_URL_CENTERGH = HY_URL_DOMAIN + "/centergh/flag?";
+	public static final String HY_URL_CIPHERURL = HY_URL_DOMAIN + "/centergh/jiami?miwen=";
+
+	public static final String WEIXIN_PUSH = "http://wx.nbrobo.com/wxPush/send.json?";
+	public static final String WEIXIN_MD5KEY = "nky_0654321";// 如果要修改 对应微信也要修改
+
 	public static final String APP_KEY = conf.getString("APP_KEY");;
-    public static final String APP_SECRET = conf.getString("APP_SECRET");;
-    public static final String TOKEN_SERVER = conf.getString("TOKEN_SERVER");;
-    public static final String CREATE_SERVER = conf.getString("CREATE_SERVER");;
-    public static final String DELETE_SERVER = conf.getString("DELETE_SERVER");;
+	public static final String APP_SECRET = conf.getString("APP_SECRET");;
+	public static final String TOKEN_SERVER = conf.getString("TOKEN_SERVER");;
+	public static final String CREATE_SERVER = conf.getString("CREATE_SERVER");;
+	public static final String DELETE_SERVER = conf.getString("DELETE_SERVER");;
 }
--- src/com/net/util/HttpParamUtil.java
+++ src/com/net/util/HttpParamUtil.java
@@ -2,145 +2,110 @@ package com.net.util;
 
 import com.net.ServiceConstants;
 
-
 /**
  * 好医网 参数拼接
+ * 
  * @author zw
  *
  */
 public class HttpParamUtil {
-	
-	
-	public static String getUrl(String hosid,String rowstart,String rowcount,String url){
-		String paramvalue="appid="+ServiceConstants.appid+"&hosid="+hosid+"&nowstp="+DateUtil.getNowStp()+"&rowcount="+rowcount+"&rowstart="+rowstart+"";
-		String a =paramvalue+"&"+ServiceConstants.key;
-//		System.out.println("加密的参数为=="+a);
-		String sign = MD5Util.encryption(a);
-		String newparam=paramvalue+"&"+"sign="+sign;
-		//添加参数
-		String rquestUrl = ServiceConstants.HOSPITALURL.replace("flag", url)+newparam;
-		System.out.println("发送请求参数==="+"url=="+rquestUrl);
-		return rquestUrl;
+
+	public static void main(String[] args) {
+		try {
+			System.out.println(getUrlForDocSchedule("8610000713", "4200271143", "860270216", "0","10"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
-	
-	public static String getUrlForDep(String deptid,String hosid,String rowstart,String rowcount,String url){
-		String paramvalue="appid="+ServiceConstants.appid+"&deptid="+deptid+"&hosid="+hosid+"&nowstp="+DateUtil.getNowStp()+"&rowcount="+rowcount+"&rowstart="+rowstart+"";
-		String a =paramvalue+"&"+ServiceConstants.key;
-//		System.out.println("加密的参数为=="+a);
-		String sign = MD5Util.encryption(a);
-		String newparam=paramvalue+"&"+"sign="+sign;
-		//添加参数
-		String rquestUrl = ServiceConstants.HOSPITALURL.replace("flag", url)+newparam;
-		System.out.println("发送请求参数==="+"url=="+rquestUrl);
+
+	public static String getUrlForAllHospital(String rowstart, String rowcount) {
+		String paramvalue = "appid=" + ServiceConstants.appid + "&hosid=0&nowstp=" + DateUtil.getNowStp() + "&rowcount="
+				+ rowcount + "&rowstart=" + rowstart + "";
+		String rquestUrl = genSignedGetUrl(ServiceConstants.HY_URL_HOSPITAL, paramvalue);
 		return rquestUrl;
 	}
 
-
-	public static String getUrlForDoc(String deptid, String docid,String hosid, String rowstart, String rowcount, String url) {
-		String paramvalue="appid="+ServiceConstants.appid+"&deptid="+deptid+"&docid="+docid+"&hosid="+hosid+"&nowstp="+DateUtil.getNowStp()+"&rowcount="+rowcount+"&rowstart="+rowstart+"";
-		String a =paramvalue+"&"+ServiceConstants.key;
-//		System.out.println("加密的参数为=="+a);
-		String sign = MD5Util.encryption(a);
-		String newparam=paramvalue+"&"+"sign="+sign;
-		//添加参数
-		String rquestUrl = ServiceConstants.DOCTORURL.replace("flag", url)+newparam;
-		System.out.println("发送请求参数==="+"url=="+rquestUrl);
-		return rquestUrl;
-		
-	}
-
-
-	public static String getUrlForPart(String doctorno,String hosid,String scheduleid, String rowstart,String rowcount, String url) {
-		String paramvalue="appid="+ServiceConstants.appid+"&doctorno="+doctorno+"&hosid="+hosid+"&nowstp="+DateUtil.getNowStp()+"&rowcount="+rowcount+"&rowstart="+rowstart+"&scheduleid="+scheduleid;
-		String a =paramvalue+"&"+ServiceConstants.key;
-//		System.out.println("加密的参数为=="+a);
-		String sign = MD5Util.encryption(a);
-		String newparam=paramvalue+"&"+"sign="+sign;
-		//添加参数
-		String rquestUrl = ServiceConstants.HOSPITALURL.replace("flag", url)+newparam;
-		System.out.println("发送请求参数==="+"url=="+rquestUrl);
+	public static String getUrlForDeptInHospital(String hosid, String rowstart, String rowcount) {
+		String paramvalue = "appid=" + ServiceConstants.appid + "&hosid=" + hosid + "&nowstp=" + DateUtil.getNowStp()
+				+ "&rowcount=" + rowcount + "&rowstart=" + rowstart + "";
+		String rquestUrl = genSignedGetUrl(ServiceConstants.HY_URL_DEPARTMENT, paramvalue);
 		return rquestUrl;
 	}
 
-
-	public static String getUrlForghlock(String hosid, String partscheduleid,String scheduleid, String url) {
-		String paramvalue="appid="+"HYGH000002"+"&hosid="+hosid+"&nowstp="+DateUtil.getNowStp()+"&partscheduleid="+partscheduleid+"&scheduleid="+scheduleid;
-		String a =paramvalue+"&"+ServiceConstants.key;
-//		System.out.println("加密的参数为=="+a);
-		String sign = MD5Util.encryption(a);
-		String newparam=paramvalue+"&"+"sign="+sign;
-		//添加参数
-		String rquestUrl = ServiceConstants.CENTERGH.replace("flag", url)+newparam;
-		System.out.println("发送请求参数==="+"url=="+rquestUrl);
-		return rquestUrl;
-		
-	}
-
-
-	public static String getUrlFororder(String orderid,String url) {
-		String paramvalue="appid="+ServiceConstants.appid+"&nowstp="+DateUtil.getNowStp()+"&orderid="+orderid;
-		String a =paramvalue+"&"+ServiceConstants.key;
-//		System.out.println("加密的参数为=="+a);
-		String sign = MD5Util.encryption(a);
-		String newparam=paramvalue+"&"+"sign="+sign;
-		//添加参数
-		String rquestUrl = ServiceConstants.CENTERGH.replace("flag", url)+newparam;
-		System.out.println("发送请求参数==="+"url=="+rquestUrl);
+	public static String getUrlForDoctorInDept(String deptid, String hosid, String rowstart, String rowcount) {
+		String paramvalue = "appid=" + ServiceConstants.appid + "&deptid=" + deptid + "&hosid=" + hosid + "&nowstp="
+				+ DateUtil.getNowStp() + "&rowcount=" + rowcount + "&rowstart=" + rowstart + "";
+		String rquestUrl = genSignedGetUrl(ServiceConstants.HY_URL_DOCTOR, paramvalue);
 		return rquestUrl;
 	}
 
+	public static String getUrlForDocSchedule(String docid, String deptid, String hosid, String rowstart,
+			String rowcount) {
+		String paramvalue = "appid=" + ServiceConstants.appid + "&deptid=" + deptid + "&docid=" + docid + "&hosid="
+				+ hosid + "&nowstp=" + DateUtil.getNowStp() + "&rowcount=" + rowcount + "&rowstart=" + rowstart + "";
+		String rquestUrl = genSignedGetUrl(ServiceConstants.HY_URL_DOCTOR_SHCEDULE, paramvalue);
+		return rquestUrl;
 
-	public static String getUrlForCancle(String cancelreason, String operator,String orderid, String url) {
-		String paramvalue="appid="+ServiceConstants.appid+"&cancelreason="+cancelreason+"&nowstp="+DateUtil.getNowStp()+"&operator="+operator+"&orderid="+orderid;
-		String a =paramvalue+"&"+ServiceConstants.key;
-//		System.out.println("加密的参数为=="+a);
-		String sign = MD5Util.encryption(a);
-		String newparam=paramvalue+"&"+"sign="+sign;
-		//添加参数
-		String rquestUrl = ServiceConstants.CENTERGH.replace("flag", url)+newparam;
-		System.out.println("发送请求参数==="+"url=="+rquestUrl);
+	}
+
+	public static String getUrlForDocParttimeSchedule(String doctorno, String hosid, String scheduleid, String rowstart,
+			String rowcount) {
+		String paramvalue = "appid=" + ServiceConstants.appid + "&doctorno=" + doctorno + "&hosid=" + hosid + "&nowstp="
+				+ DateUtil.getNowStp() + "&rowcount=" + rowcount + "&rowstart=" + rowstart + "&scheduleid="
+				+ scheduleid;
+		String rquestUrl = genSignedGetUrl(ServiceConstants.HY_URL_DOCTOR_PARTTIME_SHCEDULE, paramvalue);
 		return rquestUrl;
 	}
 
-
-	public static String getUrlForCancle(String bstp, String estp,
-			String hosid, String rowcount, String rowstart, String url) {
-		String paramvalue="appid="+ServiceConstants.appid+"&bstp="+bstp+"&estp="+estp+"&hosid="+hosid+"&nowstp="+DateUtil.getNowStp()+"&rowcount="+rowcount+"&rowstart="+rowstart;
-		String a =paramvalue+"&"+ServiceConstants.key;
-//		System.out.println("加密的参数为=="+a);
-		String sign = MD5Util.encryption(a);
-		String newparam=paramvalue+"&"+"sign="+sign;
-		//添加参数
-		String rquestUrl = ServiceConstants.DOCTORURL.replace("flag", url)+newparam;
-		System.out.println("发送请求参数==="+"url=="+rquestUrl);
+	public static String getUrlForGhLock(String hosid, String partscheduleid, String scheduleid) {
+		String paramvalue = "appid=" + "HYGH000002" + "&hosid=" + hosid + "&nowstp=" + DateUtil.getNowStp()
+				+ "&partscheduleid=" + partscheduleid + "&scheduleid=" + scheduleid;
+		String rquestUrl = genSignedGetUrl(ServiceConstants.HY_URL_LOCK, paramvalue);
 		return rquestUrl;
-		
+
 	}
-	
+
+	public static String getUrlForPlaceOrder(String orderid) {
+		String paramvalue = "appid=" + ServiceConstants.appid + "&nowstp=" + DateUtil.getNowStp() + "&orderid="
+				+ orderid;
+		String rquestUrl = genSignedGetUrl(ServiceConstants.HY_URL_PLACE_ORDER, paramvalue);
+		return rquestUrl;
+	}
+
+	public static String getUrlForCancleOrder(String cancelreason, String operator, String orderid) {
+		String paramvalue = "appid=" + ServiceConstants.appid + "&cancelreason=" + cancelreason + "&nowstp="
+				+ DateUtil.getNowStp() + "&operator=" + operator + "&orderid=" + orderid;
+		String rquestUrl = genSignedGetUrl(ServiceConstants.HY_URL_CANCEL_ORDER, paramvalue);
+		return rquestUrl;
+	}
+
+	public static String getUrlForDoctorStop(String bstp, String estp, String hosid, String rowcount, String rowstart) {
+		String paramvalue = "appid=" + ServiceConstants.appid + "&bstp=" + bstp + "&estp=" + estp + "&hosid=" + hosid
+				+ "&nowstp=" + DateUtil.getNowStp() + "&rowcount=" + rowcount + "&rowstart=" + rowstart;
+		String rquestUrl = genSignedGetUrl(ServiceConstants.HY_URL_DOCTOR_STOP, paramvalue);
+		return rquestUrl;
+
+	}
+
 	/**
 	 * 获取加密参数
-	 * @throws Exception 
+	 * 
+	 * @throws Exception
 	 */
-	public static String getCiphByString(String param) throws Exception{
-		String url =ServiceConstants.CIPHERURL+param;
-		String ciph =  HttpClient.doGet(url, "utf-8");
-		return ciph.substring(ciph.indexOf("是\"")+4, ciph.length()-2);
+	public static String getCiphByString(String param) throws Exception {
+		String url = ServiceConstants.HY_URL_CIPHER + param;
+		String ciph = HttpClient.doGet(url, "utf-8");
+		return ciph.substring(ciph.indexOf("是\"") + 4, ciph.length() - 2);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	private static String genSignedGetUrl(String url, String param) {
+		String a = param + "&" + ServiceConstants.key;
+		String sign = MD5Util.encryption(a);
+		String newparam = param + "&" + "sign=" + sign;
+		String signedUrl = url + "?" + newparam;
+		System.out.println("发送请求参数===" + "url==" + signedUrl);
+		return signedUrl;
+	}
 
 }
